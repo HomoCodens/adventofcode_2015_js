c48718964d69a500f866e170c83dcab92fdebf09
thoenis <sthoeni@kof.ethz.ch>
2020-05-24 09:07:32 +0200

    solve day 17

--- solvers/day17.js
+++ solvers/day17.js
@@ -1,3 +1,37 @@
+const isValidCombination = (mask, capacities, target) => {
+  let amount = 0;
+  let possibleHit = true;
+  for(var i = 0; i < capacities.length; i++) {
+    amount += ((mask & (1 << i)) > 0) * capacities[i];
+    possibleHit &= amount <= target;
+    if(!possibleHit) {
+      return false;
+    }
+  }
+  return amount == target;
+}
+
+const getNBits = (mask, length) => {
+  let bits = 0;
+  for(var i = 0; i < length; i++) {
+    bits += (mask & 1 << i) > 0;
+  }
+  return bits;
+}
+
 exports.solver = function(input) {
-  return null;
+  const capacities = input.split('\n').map((x) => Number.parseInt(x));
+
+  const nContainers = capacities.length;
+  const masks = new Array(2**nContainers).fill(0).map((x, i) => i);
+
+  const validCombinations = masks.filter((mask) => isValidCombination(mask, capacities, 150));
+
+  console.log(`Playing with all our containers, we can fit the eggnog ${validCombinations.length} different ways`);
+
+  let bits = validCombinations.map((mask) => getNBits(mask, nContainers));
+  let minBits = bits.reduce((acc, x) => acc > x ? x : acc);
+  let combinationsWithMinbits = validCombinations.filter((x, i) => bits[i] == minBits);
+
+  console.log(`Minimum number of containers is ${minBits} yielding ${combinationsWithMinbits.length} possible combinations.`)
 }
